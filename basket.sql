f98fc5c82ed3c2a76f9c361a6c0e8eb8e830328f
EC2 Default User <ec2-user@ip-172-31-42-188.ec2.internal>
2017-03-26 08:21:52 +0000

    Start to connect Angular to the db.

--- basket.sql
+++ basket.sql
@@ -14,20 +14,30 @@ CREATE TABLE players (
 	PRIMARY KEY (phone)
 );
 
-CREATE TABLE game (
+INSERT INTO players (phone, name, zipcode, zipRange, fun, intense, hardcore) VALUES ('5406661234', 'Johnny Basketball', 12345, 5, true, true, true);
+INSERT INTO players (phone, name, zipcode, zipRange, fun, intense, hardcore) VALUES ('1234567890', 'Michael Jordan', 12340, 5, false, false, true);
+INSERT INTO players (phone, name, zipcode, zipRange, fun, intense, hardcore) VALUES ('7031234792', 'Larry Bird', 12348, 5, true, false, true);
+
+CREATE TABLE games (
 	id serial NOT NULL PRIMARY KEY,
 	address text NOT NULL,
 	city text NOT NULL,
+        state text NOT NULL,
 	zip int NOT NULL,
 	tipOff time NOT NULL,
 	day date NOT NULL,
 	fun boolean NOT NULL,
 	intense boolean NOT NULL,
 	hardcore boolean NOT NULL
---PRIMARY KEY (id)
 );
+
+INSERT INTO games (address, city, state, zip, tipOff, day, fun, intense, hardcore) VALUES ('742 Evergreen Terrace', 'Springfield', 'VA', 12345, '12:00 PM', 'Mar-28-2017', true, false, false);
+INSERT INTO games (address, city, state, zip, tipOff, day, fun, intense, hardcore) VALUES ('500 Cherry Ave', 'Charlottesville', 'VA', 12345, '2:30 PM', 'Mar-30-2017', false, true, false);
 
 CREATE TABLE registered(
 	phone varchar(10) NOT NULL REFERENCES players(phone),
-	gameID serial NOT NULL REFERENCES game(id)
+	gameID serial NOT NULL REFERENCES games(id)
 );
+
+INSERT INTO registered (phone, gameID) VALUES ('5406661234', 1);
+INSERT INTO registered (phone, gameID) VALUES ('1234567890', 1);
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,8 @@
 var app = angular.module('ballerApp', []);
 
-var game1 = {'numPlayers': 4, 'location': '601 F St NW, Washington, DC 20004', 'time': '4:00 PM', 'intensity': 'Hardcore'};
-var game2 = {'numPlayers': 6, 'location': '1523 Someroad Rd, Fredericksburg Va', 'time': '12:00 PM', 'intensity': 'Fun'};
-var game3 = {'numPlayers': 2, 'location': '742 Evergreen Terrace, Springfield Va', 'time': '9:30 AM', 'intensity': 'Intense'};
-
 app.controller('ballerController', function($scope) {
+        
+        var socket = io.connect('https://' + document.domain + ':' + location.port + '/baller');
         
         $scope.signUp = [];
         $scope.signUp.available = {
@@ -17,18 +15,13 @@ app.controller('ballerController', function($scope) {
             'sat': true
         };
 
-        $scope.upcomingGames = [game1, game2, game3];
+        $scope.upcomingGames = undefined; 
 
         $scope.submit = function submit() {
            console.log($scope.signUp); 
         };
-        
-        $scope.getFirstAddr = function getFirstAddr() {
-            console.log($scope.upcomingGames[0].location);
-            var converted = "https://www.google.com/maps/embed/v1/search?q=";
-            converted.concat(encodeURIComponent($scope.upcomingGames[0].location.trim()));
-            converted.concat("&zoom=17&key=AIzaSyCFgAJIwC4I6oYusMKlM4yd1Z2KZiMRInU");
 
-            return converted;
-        };
+        socket.on('connect', function() {
+            console.log('Connected.');
+        });
 });
